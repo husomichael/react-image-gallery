66c255099971da811100d265630d02b6de727735
Michael Huso <husomichael@gmail.com>
2021-11-28 18:50:38 -0600

    gallerydata objects imported

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,6 +1,17 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
-    
+    { id: 2, path: 'images/76mm.jpeg', description: '76mm onboard USS Thach.', likes: 0},
+    { id: 3, path: 'images/bootcampgraduation.jpeg', description: 'Bootcamp graduation mom and I.', likes: 0},
+    { id: 4, path: 'images/bootcampweekend.jpeg', description: 'Bootcamp graduation with family.', likes: 0},
+    { id: 5, path: 'images/charlie.jpeg', description: "Mom's dog Charlie.'", likes: 0},
+    { id: 7, path: 'images/ffg43.jpeg', description: 'Forecastle ceremony on USS Thach, FFG 43.', likes: 0},
+    { id: 8, path: 'images/helopad.jpeg', description: 'Flight operations onboard USS Thach, FFG 43.', likes: 0},
+    { id: 9, path: 'images/instagram2010.jpeg', description: 'Messing with Instagram filters when it was new.', likes: 0},
+    { id: 10, path: 'images/landcruiser.JPG', description: '1999 Land Cruiser', likes: 0},
+    { id: 11, path: 'images/lynettessunflower.jpeg', description: "Lynette's sunflower at her farm.", likes: 0},
+    { id: 12, path: 'images/petrajordan.jpeg', description: 'Petra, Jordan 2009', likes: 0},
+    { id: 13, path: 'images/puntaarenas.jpeg', description: 'Punta Arenas, Chile. 2011.', likes: 0},
+    { id: 14, path: 'images/triumphmotorcycles.jpeg', description: 'Triumph Motorcycles at the coast, San Diego.', likes: 0}
 ];
+
 
 module.exports = galleryItems;
