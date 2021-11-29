16ee23ad9314db62980ab58b78529016cbe03488
Michael Huso <husomichael@gmail.com>
2021-11-28 20:17:01 -0600

    styled images to float left and have some space

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -2,7 +2,7 @@ const galleryItems = [
     { id: 2, path: 'images/76mm.jpeg', description: '76mm onboard USS Thach.', likes: 0},
     { id: 3, path: 'images/bootcampgraduation.jpeg', description: 'Bootcamp graduation mom and I.', likes: 0},
     { id: 4, path: 'images/bootcampweekend.jpeg', description: 'Bootcamp graduation with family.', likes: 0},
-    { id: 5, path: 'images/charlie.jpeg', description: "Mom's dog Charlie.'", likes: 0},
+    { id: 5, path: 'images/charlie.jpeg', description: "Mom's dog Charlie.", likes: 0},
     { id: 7, path: 'images/ffg43.jpeg', description: 'Forecastle ceremony on USS Thach, FFG 43.', likes: 0},
     { id: 8, path: 'images/helopad.jpeg', description: 'Flight operations onboard USS Thach, FFG 43.', likes: 0},
     { id: 9, path: 'images/instagram2010.jpeg', description: 'Messing with Instagram filters when it was new.', likes: 0},
